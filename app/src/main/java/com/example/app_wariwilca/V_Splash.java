faca3dfd64bcd8786f5228174291fd54841b65f5
Yeric10 <73600753@continental.edu.pe>
2023-04-27 15:55:15 -0500

    Actualizacion Yeric

---
+++ app/src/main/java/com/example/app_wariwilca/V_Splash.java
@@ -0,0 +1,32 @@
+package com.example.app_wariwilca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class V_Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Llama al layout activity -> El diseño Interfaz del Splash
+        setContentView(R.layout.activity_vsplash);
+
+         Timer tiempo;
+        TimerTask carga = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(V_Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        tiempo = new Timer();
+        tiempo.schedule(carga, 2000);
+    }
+}
