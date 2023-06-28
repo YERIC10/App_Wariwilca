60bc30c39b53576b9d52f146bf95129227b95acf
yeric10 <73600753@continental.edu.pe>
2023-06-28 14:01:19 -0500

    hola

--- app/src/main/java/com/example/app_wariwilca/ui/comentario/Comentario.java
+++ app/src/main/java/com/example/app_wariwilca/ui/comentario/Comentario.java
@@ -32,6 +32,11 @@ public class Comentario extends Fragment {
     Usuario usuario;
     ProgressBar progressBar_Comentario;
 
+    // This is an ad unit ID for a test ad. Replace with your own banner ad unit ID.
+    //private static final String AD_UNIT_ID = "ca-app-pub-3940256099942544/9214589741";
+    //private static final String TAG = "MyActivity";
+    //private FrameLayout adContainerView;
+    //private AdView adView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
